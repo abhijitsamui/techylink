5ebae4c91f9dabc01971d9e1c7db09356535a380
Abhijit Samui <abhijit.samui@gmail.com>
2018-11-06 15:02:23 -0700

    Adding client code using CRA & install consurrently to run both client and server from single command

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
 
 // @route GET api/posts/test
 // @desc Tests posts route
 // @access public
 router.get("/test", (req, res) => res.json({ msg: "posts work" }));
+
+// @route POST api/posts
+// @desc Create post
+// @access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const newPost = new Posts({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
---
+++ validation/post.js
@@ -0,0 +1,16 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+  data.text = !isEmpty(data.text) ? data.text : "";
+
+  if (Validator.isEmpty(data.text)) {
+    errors.text = "Text is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
